5aba306d2c5a3ad04662541da71a28d9a272ca92
Ezra Huang <ezrahuang155@gmail.com>
2023-11-24 02:41:07 -0500

    fix overflow ellipsis

--- components/Explorer.vue
+++ components/Explorer.vue
@@ -16,15 +16,17 @@ const makeAnchor = (id: string) => {
 <template>
   <div class="border border-white max-w-sm p-1">
     <ul>
-      <li v-for="dir in directories" class="flex items-center space-x-2">
+      <li v-for="dir in directories" class="flex items-center space-x-2 shrink-0">
         <UIcon name="i-heroicons-folder" class="w-4 h-4" />
-        <a class="whitespace-nowrap overflow-ellipsis" v-bind:href="'/files/' + directory + dir.name">{{ dir.name }}</a>
+        <a class="whitespace-nowrap overflow-ellipsis overflow-hidden" v-bind:href="'/files/' + directory + dir.name">{{
+          dir.name }}</a>
       </li>
     </ul>
     <ul>
       <li v-for="file in files" class="flex items-center space-x-2">
-        <UIcon name="i-heroicons-document" class="w-4 h-4 text-white" />
-        <a class="whitespace-nowrap overflow-ellipsis" v-bind:href='makeAnchor(file.id)'>{{ file.name }}</a>
+        <UIcon name="i-heroicons-document" class="w-4 h-4 text-white shrink-0" />
+        <a class="whitespace-nowrap overflow-ellipsis overflow-hidden" v-bind:href='makeAnchor(file.id)'>{{ file.name
+        }}</a>
       </li>
     </ul>
   </div>
